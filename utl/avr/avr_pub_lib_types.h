74ca7885e7b6fa960972ce422167ca8c653f3622
Brandon Pfeifer <thebpfeifer@gmail.com>
2016-03-15 20:43:31 -0500

    -Added common avr types and debug printing files

---
+++ utl/avr/avr_pub_lib_types.h
@@ -0,0 +1,20 @@
+/*
+ * avr_pub_lib_types.h
+ *
+ * Created: 3/15/2016 8:05:23 PM
+ *  Author: Brandon Pfeifer 
+ *
+ *	This file holds all the common type definitions for AVR microcontrollers. 
+ *	The purpose of these definitions is to abstract the code from any AVR-specific
+ *	type definitions. 
+ */ 
+
+
+#ifndef AVR_PUB_LIB_TYPES_H_
+#define AVR_PUB_LIB_TYPES_H_
+
+#define boolean uint8_t
+#define	FALSE	0
+#define TRUE	1
+
+#endif /* AVR_PUB_LIB_TYPES_H_ */
